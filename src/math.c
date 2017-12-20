b4b8cb7fefa3a4a8a6749c3cd714711e417de8f9
chenshuijin <785795635@qq.com>
2017-12-20 16:04:19 +0800

    add mulfunc

--- src/math.c
+++ src/math.c
@@ -41,3 +41,67 @@ char* add(char *l, char *r, int base)
 
   return result;
 }
+
+char* muli(char *dest, char *l, int r, int base)
+{
+  int len = 0, i = 0, tmp = 0, c = 0;
+  char *result;
+
+  if (r == 0) return "0";
+
+  lstrip(l, '0');
+  len = strlen(l);
+  if (len == 0) return "0";
+
+  reverse(l);
+  memset(dest, 0, len+2);
+
+  i = 0; tmp = 0; c = 0;
+  while(1) {
+    if (i >= len) {
+      if (c <= 0) break;
+    }
+    else tmp = r * ctoi(*(l+i));
+
+    tmp += c;
+    c = tmp/base;
+    tmp %= base;
+    *(dest + i) = itoc(tmp, base);
+    i ++;
+    tmp = 0;
+  }
+  reverse(l);
+  reverse(dest);
+  lstrip(dest, '0');
+  return dest;
+}
+
+char* mul(char *l, char *r, int base)
+{
+  int len=0, i = 0, llen = 0, c = 0, tmp = 0;
+  char *result, *s, *ms, tmpc;
+  lstrip(l, '0');lstrip(r, '0');
+  if (strlen(l) < strlen(r)) return mul(r, l, base);
+
+  reverse(r);
+
+  len = strlen(r);
+  llen = strlen(l);
+
+  if ((s=(char*)malloc(len+llen+2))==NULL)return NULL;
+  memset(s, 0, len+llen+2);
+  if ((ms=(char*)malloc(len+llen+2))==NULL)return NULL;
+  memset(ms, 0, len+llen+2);
+
+  i = 0, c = 0, tmp = 0;
+  result = "0";
+  while(i<len){
+    s = rpad(s, l, '0', llen + i);
+    muli(ms, s, ctoi(*(r+i)), base);
+    result = add(result, ms, base);
+    i++;
+  }
+  reverse(r);
+  lstrip(result, '0');
+  return result;
+}
--- src/math.h
+++ src/math.h
@@ -1,4 +1,5 @@
 #ifndef MATH_H
 #define MATH_H
 char* add(char *l, char *r, int base);
+char* mul(char *l, char *r, int base);
 #endif
